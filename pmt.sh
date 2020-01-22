4852b7bd3ffb626b0ac66a68c6bc08e680ecc51d
Mufti05 <59869552+Mufti05@users.noreply.github.com>
2020-01-23 05:33:43 +0700

    Add files via upload

---
+++ pmt.sh
@@ -0,0 +1,106 @@
+#!bin/sh
+aa='\033[34;1m'
+bb='\033[32;1m'
+cc='\033[35;1m'
+dd='\033[36;1m'
+ee='\033[31;1m'
+ff='\033[37;m'
+gg='\033[33;m'
+
+clear
+screenfetch
+sleep 3
+echo $aa"Sopo Jenengmu"
+read nm
+clear
+echo $bb"kesuwon"
+sleep 2
+clear
+figlet $nm
+sleep 1
+figlet Mufti
+sleep 1
+figlet Pamit
+sleep 1
+figlet Teko
+sleep 1
+figlet Dunia
+sleep 1
+figlet Hackers
+sleep 1
+echo $cc"[1].Opo'o"
+echo $dd"[0].metu aku wes"
+read pl
+if [ $pl = 1 ]
+then
+    echo $ee"_______                                              |   _   .-----.---.-.--------.
+         |   1___|  _  |  _  |        |
+         |____   |   __|___._|__|__|__|
+         |:  1   |__| +-+-+-+-+-+-+-+-+
+         |::.. . |    |M|U|F|T|U|0|5|★|
+         '-------'    +-+-+-+-+-+-+-+-+
+         ══════════════════════════════
+         Author: Mr.KILLERS (Mufti05)
+         Spcial Team: Legends Owl Squad
+         ══════════════════════════════
+             </> Pamit Dari... </>
+         ══════════════════════════════
+
+         ╔════════════════════════╗
+         ║BY MUFTI:PESAN "
+    sleep 3
+    echo $ff"Aku Selaku Admin"
+    sleep 1
+    echo $aa"Legends Owl Squad"
+    sleep 1
+    echo $bb"Jalok Sepuro"
+    sleep 1
+    echo $cc"Aku vakum dari Dunia Hacker"
+    sleep 1
+    echo $dd"Aku ws Kesel $nm"
+    sleep 1
+    echo $ee"Aku Kessel Dadi Hacker"
+    sleep 1
+    echo $ff"Wes Aku Leren Dadi Hacker"
+    sleep 1
+    echo $aa"aku tetep Grub"
+    sleep 1
+    echo $bb"Legends Owl Squad"
+    sleep 1
+    echo $gg"[1]Ojok"
+    echo $dd"[2]Iyo Wes"
+    read ks
+fi
+if [ $ks = 1 ]
+then
+    echo $aa"Tapi aku kesel $nm"
+    echo $ff"Aku kate leren Wes teko dunia hacker"
+    echo $cc"4.iyo ws"
+    read pr
+fi
+if [ $ks = 2 ]
+then
+    clear
+    echo $bb"Tapi"
+    sleep 1
+    clear
+    echo $aa"Tapi"
+    sleep 1
+    clear
+    echo $ee"Tapi"
+    sleep 3
+    figlet Boong
+fi
+if [ $pr = 4 ]
+then
+    clear
+    echo $aa"Tapi"
+    sleep 1
+    clear
+    echo $gg"Tapi"
+    sleep 1
+    clear
+    echo $cc"Tapi"
+    sleep 3
+    figlet Boong
+fi
